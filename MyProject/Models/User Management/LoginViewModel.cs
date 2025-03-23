b728648060e8423bee084e826ca84b4dcb5f9d76
rjcrohitt11sci368@gmail.com <rjcrohitt11sci368@gmail.com>
2025-03-23 14:49:30 +0530

    updated authentication

---
+++ MyProject/Models/User Management/LoginViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MyProject.Models.User_Management
+{
+    public class LoginViewModel
+    {
+        [Required]
+        public string Email { get; set; }
+
+
+        [Required, DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
--- MyProject/Program.cs
+++ MyProject/Program.cs
@@ -1,5 +1,7 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 using MyProject.Models;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,6 +10,24 @@ builder.Services.AddControllersWithViews();
 var provider = builder.Services.BuildServiceProvider();
 var config = provider.GetRequiredService<IConfiguration>();
 builder.Services.AddDbContext<LaCaffeineContext>(item => item.UseSqlServer(config.GetConnectionString("dbcs")));
+
+builder.Services.AddAuthentication("Bearer")
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,    
+            ValidIssuer = builder.Configuration["JwtSettings:Issuer"],
+            ValidAudience = builder.Configuration["JwtSettings:Audience"],
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["JwtSettings:SecretKey"]))
+            
+        };
+
+    });
+
 var app = builder.Build();
 
 app.UseStaticFiles();
